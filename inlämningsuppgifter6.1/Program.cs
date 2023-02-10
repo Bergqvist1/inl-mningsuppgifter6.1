7376520924c01d7a77cfd2cb236791cfc169cb38
Rittza <elba05002@utb.vaxjo.se>
2023-02-10 10:22:33 +0100

    Add project files.

---
+++ inlämningsuppgifter6.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_6_1
+{
+    class Program
+    {
+        static void Addera(int tal1, int tal2, int tal3)
+        {
+            Console.WriteLine($"Dina 3 tal blir adderat: {tal1+tal2+tal3}");
+            return;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Addera(1, 4, 6);
+            
+        }
+
+        
+    }
+}
+
